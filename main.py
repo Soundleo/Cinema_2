c4f0d6427b1b868297a5f202bedb43e3436862c8
Soundleo <leorperes@gmail.com>
2024-05-22 21:29:53 -0300

    Atividade 04 - Versão 1.0

---
+++ main.py
@@ -0,0 +1,93 @@
+# Programa de do cinema Com idade
+
+print('******************************************************************************************************************************************')
+print('******************************************************************************************************************************************')
+print('Para informar qual sua sala seu file vai passar precisamos de alguns dados para oferecer uma boa experencia para vc!')
+
+import os 
+import time
+time.sleep(2)
+nome = input('informe o seu nome: \n')
+idade = int(input('informe a sua idade \n'))
+
+while True:
+
+    print(f'Querido cliente {nome} nosso cinema possui 5 salas, qual filme o senhor comprou o ingresso:')
+    print('1 - Ataque dos vermes maldidos, classificação indicativa: Livre')
+    print('2 - Tomates verdes fritos, classificação indicativa: 12 anos')
+    print('3 - Rollerball, classificação indicativa: 14 anos')
+    print('4 - Marte ataca, classificação indicativa: 16 anos')
+    print('5 - Não olhe para Cima, classificação indicativa: 18 anos')
+
+    sala = input('Informe qual o filme o senhor selecionou 1 a 5: ')
+    
+    match sala:
+        case '1': 
+            if idade > 0:
+                os.system('cls')
+                print (f'Senhor {nome} o seu filme será exibido na sala 1. ')
+                print ('Tenha um bom filme, aproveite compre uma pipoca.')
+                time.sleep(10)
+                os.system('cls')
+                break
+            else:
+                os.system('cls')
+                print('ENTRADA NÃO PERMITIDA')
+                time.sleep(3)
+                os.system('cls')
+        case '2':
+            if idade >= 12:
+                os.system('cls)')
+                print (f'Senhor {nome} o seu filme será exibido na sala 2. ')
+                print ('Tenha um bom filme, aproveite compre uma pipoca.')
+                time.sleep(10)
+                os.system('cls')
+                break
+            else:
+                os.system('cls')
+                print('ENTRADA NÃO PERMITIDA')
+                time.sleep(10)
+                os.system('cls') 
+        case '3':
+            if idade >= 14:
+                os.system('cls')
+                print (f'Senhor {nome} o seu filme será exibido na sala 3 ')
+                print ('Tenha um bom filme, aproveite compre uma pipoca.')
+                time.sleep(10)
+                os.system('cls')
+                break
+            else:
+                os.system('cls')
+                print('ENTRADA NÃO PERMITIDA')
+                time.sleep(10)
+                os.system('cls')  
+        case '4': 
+            if idade >= 16:
+                os.system('cls')
+                print (f'Senhor {nome} o seu filme será exibido na sala 4 ')
+                print ('Tenha um bom filme, aproveite compre uma pipoca.')
+                time.sleep(10)
+                os.system('cls')  
+                break
+            else:
+                os.system('cls')
+                print('ENTRADA NÃO PERMITIDA')
+                time.sleep(10)
+                os.system('cls')
+        case '5':
+            if idade >= 18:
+                os.system('cls')
+                print (f'Senhor {nome} o seu filme será exibido na sala 5 ')
+                print ('Tenha um bom filme, aproveite compre uma pipoca. Para este filme temos uma proção no combo de pipoca e refri')
+                time.sleep(10)
+                os.system('cls') 
+                break
+            else:
+                os.system('cls')
+                print('ENTRADA NÃO PERMITIDA')
+                time.sleep(10)
+                os.system('cls')
+        case _:
+            print('opçao inválida')
+            
+            continue
